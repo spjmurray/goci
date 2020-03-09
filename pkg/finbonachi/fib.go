4dc3fa8df4e1731ac0d4253c2d9a346cb9b652a4
Simon Murray <spjmurray@yahoo.co.uk>
2020-03-09 09:24:28 +0000

    Test ascii doc

--- pkg/finbonachi/fib.go
+++ pkg/finbonachi/fib.go
@@ -11,11 +11,11 @@ func Fib(n int) (int, error) {
 	if n == 1 || n == 2 {
 		return 1, nil
 	}
-	a, err := Fib(n-1)
+	a, err := Fib(n - 1)
 	if err != nil {
 		return 0, err
 	}
-	b, err := Fib(n-2)
+	b, err := Fib(n - 2)
 	if err != nil {
 		return 0, err
 	}
